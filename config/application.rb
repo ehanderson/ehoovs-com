25b2629df82466be811e5d2b7eacfde7270248b5
Mac Owner <macowner@Macs-MacBook-Pro.local>
2014-01-14 11:57:29 -0600

    heroku config/app changes

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,8 @@ if defined?(Bundler)
 end
 
 module MyBlog
-  class Application < Rails::Application
+  class Application < Rails::
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
