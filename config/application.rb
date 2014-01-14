f84f6c5b085635559961e2cd788285e7965d34da
Mac Owner <macowner@Macs-MacBook-Pro.local>
2014-01-14 12:34:49 -0600

    heroku added n in the config file

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ end
 
 
 module MyBlog
-  class Application < Rails::
+  class Application < Rails::Application
     # config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
